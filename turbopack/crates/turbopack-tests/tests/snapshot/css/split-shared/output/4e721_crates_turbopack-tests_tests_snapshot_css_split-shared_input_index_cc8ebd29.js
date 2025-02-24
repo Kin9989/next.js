84409de96b9d703ec44e73035f1642955459b59f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-02-24 18:36:16 +0100

    Update snapshots

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/css/split-shared/output/4e721_crates_turbopack-tests_tests_snapshot_css_split-shared_input_index_cc8ebd29.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4e721_crates_turbopack-tests_tests_snapshot_css_split-shared_input_index_cc8ebd29.js",
+    {},
+    {"otherChunks":["output/turbopack_crates_turbopack-tests_tests_snapshot_css_split-shared_input_19740676._.js","output/4e721_crates_turbopack-tests_tests_snapshot_css_split-shared_input_index_fc583b08.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/css/split-shared/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
